1898a1aa4fc7542435518c268833c6fbb4bd2726
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-10-03 11:25:30 +0530

    Spring boot boiler plate created

---
+++ src/test/java/com/masai/PaymentWalletRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
